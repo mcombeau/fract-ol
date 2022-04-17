ccbb8dc95f3a6e3a2ef97e9c68b84a8e34752bbb
Mia <miacombeau@protonmail.com>
2022-04-17 15:54:00 +0200

    norminette

--- libft/ft_ishexdigit.c
+++ libft/ft_ishexdigit.c
@@ -1,19 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*   ft_ishexdigit.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mcombeau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mcombeau <mcombeau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 13:53:06 by mcombeau          #+#    #+#             */
-/*   Updated: 2021/12/02 14:40:52 by mcombeau         ###   ########.fr       */
+/*   Updated: 2022/04/17 15:52:08 by mcombeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 /*
 	DESCRIPTION :
-	The function ft_ishexdigit checks whether c is a hexadecimal digit character or not.
+	The function ft_ishexdigit checks whether c is a hexadecimal
+	digit character or not.
 
 	RETURN VALUE:
 	Non-zero if c is a hexadecimal digit, zero if not.
@@ -22,9 +23,9 @@ int	ft_ishexdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (c);
-    c = ft_toupper(c);
-    if (c >= 'A' && c <= 'F')
-        return (c);
-    else
-	    return (0);
+	c = ft_toupper(c);
+	if (c >= 'A' && c <= 'F')
+		return (c);
+	else
+		return (0);
 }
